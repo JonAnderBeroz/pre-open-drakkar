4e23e83daa4b84a94091b11722c393daabbb746e
Jon Ander <jonanberoz2000@gmail.com>
2023-11-23 09:51:43 +0100

    fix: text card props

--- src/components/text-card.tsx
+++ src/components/text-card.tsx
@@ -3,7 +3,7 @@ export default function TextCard({
   className = "p-10 md:w-[500px] w-[335px]",
 }: {
   children: React.ReactNode;
-  className: string;
+  className?: string;
 }) {
   return (
     <article className={`bg-neutral-400/60 backdrop-blur-sm rounded-lg  ${className}`}>
